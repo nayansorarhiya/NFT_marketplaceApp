e2252c1463e83de80c886cc9fb9b086a90a021d9
Nayan1002 <nayansorathiya1002@gmail.com>
2022-04-25 20:27:28 +0530

    sorting pending

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,6 +1,6 @@
 import { Box, TableCell, TableRow } from '@mui/material'
 import React from 'react';
-import {Skeleton } from '@mui/material';
+import { Skeleton } from '@mui/material';
 
 export const TableSkeleton = () => {
     return (
@@ -14,11 +14,11 @@ export const TableSkeleton = () => {
                     </Box>
                 </TableCell>
                 <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: 'flex', justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
-                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: 'flex', justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
-                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: 'flex', justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
-                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: 'flex', justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
-                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: 'flex', justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
-                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: 'flex', justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
+                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: { xs: 'none', sm: 'flex', md: 'flex', lg: 'flex' }, justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
+                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex' }, justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
+                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex' }, justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
+                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex' }, justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
+                <TableCell sx={{ pl: 0, pr: 0 }}><Box sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex' }, justifyContent: 'end' }}><Skeleton variant="text" width={'75%'} /></Box></TableCell>
             </TableRow>
         </>
     )
