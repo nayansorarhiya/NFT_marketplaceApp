43008af34388f36f916d4b00301440c5f1a3f0f7
ronit9586 <35962550+ronit9586@users.noreply.github.com>
2022-07-24 01:04:43 +0530

    Change smart contract

--- src/contract/contract.js
+++ src/contract/contract.js
@@ -9,7 +9,7 @@ export default {
     // diamondSwap: "0x344a77e3cAa9F0887041B9ca60B5361Ba3A74342", // delegatecall mainnet
 
     //testing
-    diamondSwap: "0x8520fe724B33A03D7425726B07489fA02C9B55C3", // call mainnet
+    diamondSwap: "0xc8b095a5c25c3f717b33ac4c2fa34d8688ec99c6", // call mainnet
     gemSwap: "0x83C8F28c26bF6aaca652Df1DbBE0e1b56F8baBa2",
 
 };
